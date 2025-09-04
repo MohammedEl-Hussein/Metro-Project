977c8d12d6f27d91295145ce11b7ae658201bfd1
MohammedEl-Hussein <162710648+MohammedEl-Hussein@users.noreply.github.com>
2025-09-04 16:18:11 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:depi/home_page.dart';
+import 'package:depi/travel_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TravelPage(),
+    );
+  }
+}
